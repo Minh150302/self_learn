ca891b206ea5f23a99c3a032ba05a4db14bbd435
LAPTOP-MINH\Huy Minh <hoanghuyminh1503@gmail.com>
2025-11-18 20:18:52 +0700

    drop out of circle

--- Bouncing Balls/Bouncing Balls.py
+++ Bouncing Balls/Bouncing Balls.py
@@ -2,6 +2,24 @@ import pygame
 import numpy as np
 import math
 
+def is_ball_in_arc(ball_pos, CIRCLE_CENTER, start_angle, end_angle):
+    dx = ball_pos[0] - CIRCLE_CENTER[0]
+    dy = ball_pos[1] - CIRCLE_CENTER[1]
+    ball_angle = math.atan2(dy, dx)
+    start_angle = start_angle % (2 * math.pi)
+    end_angle = end_angle % (2 * math.pi)
+    if start_angle > end_angle:
+        end_angle += 2 * math.pi
+
+    if start_angle <= ball_angle <= end_angle or start_angle <= ball_angle + 2 * math.pi <= end_angle:
+        return True
+
+def draw_arc(window, center, radius, start_angle, end_angle):
+    p1 = center + (radius + 1000) * np.array([math.cos(start_angle), math.sin(start_angle)])
+    p2 = center + (radius + 1000) * np.array([math.cos(end_angle), math.sin(end_angle)])
+    pygame.draw.polygon(window, BLACK, [center, p1, p2], 0)
+
+
 
 pygame.init()
 WIDTH = 600
@@ -15,9 +33,11 @@ CIRCLE_CENTER = np.array([WIDTH/2, HEIGHT/2], dtype = np.float64)
 CIRCLE_RADIUS = WIDTH/3
 BALL_RADIUS = 5
 ball_pos = np.array([WIDTH/2, HEIGHT/2 - HEIGHT/6], dtype = np.float64)
-
-
-
+arc_degrees = 60
+start_angle = math.radians(-arc_degrees/2)
+end_angle = math.radians(arc_degrees/2)
+spinning_speed = 0.01
+is_ball_in = True
 
 running = True
 GRAVITY = 0.2
@@ -29,23 +49,28 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             
-            
+    start_angle += spinning_speed
+    end_angle += spinning_speed   
     ball_vel[1] += GRAVITY 
     ball_pos += ball_vel 
 
     dist = np.linalg.norm(ball_pos - CIRCLE_CENTER)
     if dist + BALL_RADIUS > CIRCLE_RADIUS:
-        d = ball_pos - CIRCLE_CENTER
-        d_unit = d/np.linalg.norm(d)
-        ball_pos = CIRCLE_CENTER + (CIRCLE_RADIUS - BALL_RADIUS) * d_unit
-        
-        t = np.array([-d[1],d[0]], dtype = np.float64)
-        proj_v_t = (np.dot(ball_vel, t)/np.dot(t,t)) * t
-        ball_vel = 2* proj_v_t - ball_vel
-    
+        if is_ball_in_arc(ball_pos, CIRCLE_CENTER, start_angle, end_angle):
+            is_ball_in = False
+        if is_ball_in:
+            d = ball_pos - CIRCLE_CENTER
+            d_unit = d/np.linalg.norm(d)
+            ball_pos = CIRCLE_CENTER + (CIRCLE_RADIUS - BALL_RADIUS) * d_unit
+            
+            t = np.array([-d[1],d[0]], dtype = np.float64)
+            proj_v_t = (np.dot(ball_vel, t)/np.dot(t,t)) * t
+            ball_vel = 2* proj_v_t - ball_vel
+            ball_vel += t * spinning_speed
     
     window.fill(BLACK)
     pygame.draw.circle(window, ORANGE, CIRCLE_CENTER, CIRCLE_RADIUS, 3)
+    draw_arc(window, CIRCLE_CENTER, CIRCLE_RADIUS, start_angle, end_angle)
     pygame.draw.circle(window, RED, ball_pos, BALL_RADIUS )
     
     pygame.display.flip()
